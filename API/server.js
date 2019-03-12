0fc0fa08efd2a3cad806ee607dc1de150daee10f
Gagulo <grzegorz.sokalski@hotmail.com>
2019-03-12 18:12:21 +0100

    adding crud operation on backend

--- API/server.js
+++ API/server.js
@@ -2,19 +2,22 @@ const express = require('express'),
     path = require('path'),
     bodyParser = require('body-parser'),
     cors = require('cors'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    config = require('./DB');
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.DB, { useNewUrlParser: true }).then(
-      () => {console.log('Database is connected') },
-      err => { console.log('Can not connect to the database'+ err)}
-    );
+const businessRoute = require('./routes/business.route');
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
 
-    const app = express();
-    app.use(bodyParser.json());
-    app.use(cors());
-    let port = process.env.PORT || 4200;
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/business', businessRoute);
+const port = process.env.PORT || 4200;
 
-    const server = app.listen(function(){
-        console.log('Listening on port ' + port);
-    });
+const server = app.listen(port, function(){
+  console.log('Listening on port ' + port);
+});
